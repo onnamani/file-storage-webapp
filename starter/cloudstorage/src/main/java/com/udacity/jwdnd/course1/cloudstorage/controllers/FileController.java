2c13b19b0c1ed361be06df2ae070421668a01558
Obinna <68425897+onnamani@users.noreply.github.com>
2021-01-12 21:46:14 +0100

    adding file view/download & delete GET methods

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileController.java
@@ -1,11 +1,18 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,6 +28,29 @@ public class FileController {
         this.userService = userService;
     }
 
+    @GetMapping("/home/file/delete/{fileId}")
+    public String deleteFile(Model model, @PathVariable("fileId") Integer fileId) {
+        Integer deleteFile = fileService.deleteFile(fileId);
+
+        if (deleteFile == null) {
+            model.addAttribute("fileError", "Oooops..Something went wrong during deletion. Try again.");
+            return "result";
+        }
+
+        model.addAttribute("success", true);
+        return "result";
+    }
+
+    @GetMapping("/home/file/view/{fileId:.+}")
+    public ResponseEntity<ByteArrayResource> downloadFile(Model model, @PathVariable("fileId") Integer fileId) {
+        File file = fileService.getFileById(fileId);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(file.getContentType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachement; filename=\"" + file.getFilename() + "\"")
+                .body(new ByteArrayResource(file.getFilePayload()));
+    }
+
     @PostMapping("/home/file/newFile")
     public String uploadFile(
             @RequestParam("fileUpload") MultipartFile multipartFile,
@@ -28,7 +58,7 @@ public class FileController {
             Model model) {
 
         if (multipartFile.isEmpty()) {
-            model.addAttribute("fileUploadError", "You must select a file to upload");
+            model.addAttribute("fileError", "You must select a file to upload");
             return "result";
         }
 
@@ -48,12 +78,12 @@ public class FileController {
                 return "result";
 
             } catch (Exception e) {
-                model.addAttribute("fileUploadError", e.getMessage());
+                model.addAttribute("fileError", e.getMessage());
                 return "result";
             }
 
         } else {
-            model.addAttribute("fileUploadError", "Kindly provide unique file name");
+            model.addAttribute("fileError", "Kindly provide unique file name");
             return "result";
         }
     }
