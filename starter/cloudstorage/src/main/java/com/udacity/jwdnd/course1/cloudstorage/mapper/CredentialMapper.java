b1a27671b066b3baeead0bad1d0b5fbf3b989a73
Obinna <68425897+onnamani@users.noreply.github.com>
2021-01-15 11:03:11 +0100

    created Credential model class and CredentialMapper Interface

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
@@ -0,0 +1,21 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface CredentialMapper {
+
+    @Select("SELECT * FROM CREDENTIALS WHERE userid = #{userid}")
+    List<Credential> getCredentials(Integer userid);
+
+    @Insert("INSERT INTO CREDENTIALS (url, username, key, password, userid) " +
+            "VALUES(#{url}, #{username}, #{key}, #{password}, #{userid})")
+    @Options(useGeneratedKeys = true, keyProperty = "credentialid")
+    Integer saveCredential(Credential credential);
+
+    @Delete("DELETE * FROM CREDENTIALS WHERE credentialid = #{credentialid}")
+    Integer deleteCredential(Integer credentialid);
+}
